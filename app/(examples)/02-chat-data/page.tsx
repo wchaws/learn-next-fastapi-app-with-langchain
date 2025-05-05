fea7c4f66a6a88fed9f6c314efa42c506504f356
wch <wch@amazon.com>
2025-05-05 16:52:55 +0800

    chore: update chat-data

--- app/(examples)/02-chat-data/page.tsx
+++ app/(examples)/02-chat-data/page.tsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import { Card } from '@/app/components';
-import { useChat } from '@ai-sdk/react';
-import { GeistMono } from 'geist/font/mono';
+import { Card } from "@/app/components";
+import { useChat } from "@ai-sdk/react";
+import { GeistMono } from "geist/font/mono";
 
 export default function Page() {
   const { messages, input, handleSubmit, handleInputChange, status } = useChat({
-    streamProtocol: 'data',
+    streamProtocol: "data",
     maxSteps: 5,
   });
 
   return (
     <div className="flex flex-col gap-2">
       <div className="flex flex-col gap-2 p-4">
-        {messages.map(message => (
+        {messages.map((message) => (
           <div key={message.id} className="flex flex-row gap-2">
             <div className="flex-shrink-0 w-24 text-zinc-500">{`${message.role}: `}</div>
 
@@ -21,7 +21,27 @@ export default function Page() {
               {message.content && <div>{message.content}</div>}
 
               <div className="flex flex-row gap-2">
-                {message.toolInvocations?.map(toolInvocation => (
+                {message.parts.map((part, index) => {
+                  const { type } = part;
+                  if (type === "tool-invocation") {
+                    const { toolInvocation } = part;
+                    const { toolName, toolCallId, state, args, step } =
+                      toolInvocation;
+
+                    return (
+                      <pre
+                        key={toolCallId}
+                        className={`${GeistMono.className} text-sm text-zinc-500 bg-zinc-100 p-3 rounded-lg`}
+                      >
+                        <b>STEP #{1 + (step || 0)}:</b><br />
+                        {`${toolName}(${JSON.stringify(args, null, 2)})`}
+                      </pre>
+                    );
+                  }
+
+                  return <></>;
+                })}
+                {/* {message.toolInvocations?.map((toolInvocation) => (
                   <pre
                     key={toolInvocation.toolCallId}
                     className={`${GeistMono.className} text-sm text-zinc-500 bg-zinc-100 p-3 rounded-lg`}
@@ -29,10 +49,10 @@ export default function Page() {
                     {`${toolInvocation.toolName}(${JSON.stringify(
                       toolInvocation.args,
                       null,
-                      2,
+                      2
                     )})`}
                   </pre>
-                ))}
+                ))} */}
               </div>
             </div>
           </div>
@@ -50,7 +70,7 @@ export default function Page() {
           placeholder="What's the weather in San Francisco?"
           onChange={handleInputChange}
           className="w-full p-4 bg-transparent outline-none"
-          disabled={status !== 'ready'}
+          disabled={status !== "ready"}
         />
       </form>
     </div>
